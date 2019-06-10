398b5ed91bc840bd36230867b87c74e6debb2dc4
Adam <adamlobocki@gmail.com>
2019-06-11 00:33:29 +0200

    Wyjątki kontrolowane

--- src/Person.java
+++ src/Person.java
@@ -40,21 +40,21 @@ public class Person {
         this.pesel = pesel;
     }
 
-    public void add()throws NameUndefinedException{
+    public void add() throws NameUndefinedException, IncorrectAgeException {
         System.out.println("Podaj imie");
         firstName = scanner.nextLine();
         if(firstName == null || firstName.length() <= 2){
-            throw new NameUndefinedException();
+            throw new NameUndefinedException("Imię powinn0 miec minimum 3 znaki.\"");
         }
         System.out.println("Podaj nazwisko");
         lastname = scanner.nextLine();
         if (lastname == null || lastname.length() <= 2){
-            throw new NameUndefinedException();
+            throw new NameUndefinedException("Nazwisko powinno miec minimum 3 znaki.");
         }
         System.out.println("Podaj wiek");
         age = scanner.nextInt();
         if( age < 1){
-            throw new IncorrectAgeException();
+            throw new IncorrectAgeException("Wiek nie może być poniżej roku.");
         }
         scanner.nextLine();
         System.out.println("Podaj pesel");
