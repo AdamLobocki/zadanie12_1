f44422226c237be127afdb33b03b84d4a27c8189
Adam <adamlobocki@gmail.com>
2019-06-12 14:08:04 +0200

    Wyjątki kontrolowane Zadanie poprawione

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) throws NameUndefinedException, IncorrectAgeException {
         Person person = new Person();
+        Scanner scanner = new Scanner(System.in);
 
-       person.add();
+        System.out.println("Podaj Imię, Nazwisko, pesel oraz wiek");
+
+       person.add(scanner.nextLine(), scanner.nextLine(), scanner.nextLine(), scanner.nextInt());
 
 
         System.out.println(person.getFirstName() + " " + person.getLastname() + " " + person.getAge() + " " + person.getPesel());
--- src/Person.java
+++ src/Person.java
@@ -6,59 +6,45 @@ public class Person {
     private int age;
     private String pesel;
 
-    Scanner scanner = new Scanner(System.in);
+
 
     public String getFirstName() {
         return firstName;
     }
 
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
 
     public String getLastname() {
         return lastname;
     }
 
-    public void setLastname(String lastname) {
-        this.lastname = lastname;
-    }
+
 
     public int getAge() {
         return age;
     }
 
-    public void setAge(int age) {
-        this.age = age;
-    }
+
 
     public String getPesel() {
         return pesel;
     }
 
-    public void setPesel(String pesel) {
-        this.pesel = pesel;
-    }
 
-    public void add() throws NameUndefinedException, IncorrectAgeException {
-        System.out.println("Podaj imie");
-        firstName = scanner.nextLine();
+    public void add(String firstN, String lastN, String p, int a) throws NameUndefinedException, IncorrectAgeException {
+
+        firstName = firstN;
         if(firstName == null || firstName.length() <= 2){
-            throw new NameUndefinedException("Imię powinn0 miec minimum 3 znaki.\"");
+            throw new NameUndefinedException("Imię powinno miec minimum 3 znaki.\"");
         }
-        System.out.println("Podaj nazwisko");
-        lastname = scanner.nextLine();
+        lastname = lastN;
         if (lastname == null || lastname.length() <= 2){
             throw new NameUndefinedException("Nazwisko powinno miec minimum 3 znaki.");
         }
-        System.out.println("Podaj wiek");
-        age = scanner.nextInt();
+        age = a;
         if( age < 1){
             throw new IncorrectAgeException("Wiek nie może być poniżej roku.");
         }
-        scanner.nextLine();
-        System.out.println("Podaj pesel");
-        pesel = scanner.nextLine();
+        pesel = p;
 
 
 
